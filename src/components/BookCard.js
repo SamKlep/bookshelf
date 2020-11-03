2656686d4d91a25b909300415bc5231dfe2cd102
SamKlep <shamdemann23@gmail.com>
2020-11-03 17:35:23 -0600

    Card colums.

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -2,20 +2,29 @@ import React from 'react'
 
 const BookCard = (props) => {
   return (
-    <div className='card mx-auto text-center'>
-      <img className='card-img' src={props.image} alt='' />
-      <div className='desc'>
-        <div className='card-title'>
-          <h2>{props.title}</h2>
-        </div>
-        <div className='card-body'>
-          <h3>Author: {props.author}</h3>
-          <p>
-            Published Date:{' '}
-            {props.published === '0000'
-              ? 'Not available'
-              : props.published.substring(0, 4)}
-          </p>
+    <div className='container'>
+      <div className='row'>
+        <div>
+          <div className='card mx-auto text-center'>
+            <img className='card-img' src={props.image} alt='' />
+            <div className='desc'>
+              <div className='card-title'>
+                <h2>
+                  <strong>{props.title}</strong>
+                </h2>
+                <h4>{props.subtitle}</h4>
+              </div>
+              <div className='card-body'>
+                <h3>Author: {props.author}</h3>
+                <p>
+                  Published Date:{' '}
+                  {props.published === '0000'
+                    ? 'Not available'
+                    : props.published.substring(0, 4)}
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,13 +3,14 @@ import BookCard from './BookCard'
 
 const BookList = (props) => {
   return (
-    <div className='container cards'>
+    <div className='container card-columns'>
       {props.books.map((book, i) => {
         return (
           <BookCard
             key={i}
             image={book.volumeInfo.imageLinks.thumbnail}
             title={book.volumeInfo.title}
+            subtitle={book.volumeInfo.subtitle}
             author={book.volumeInfo.authors}
             published={book.volumeInfo.publishedDate}
           />
